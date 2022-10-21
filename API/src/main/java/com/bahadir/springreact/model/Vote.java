ef98c17035006e9cb7b6e7958a7123117f2a5b15
barisbahadir <barisbahadir@live.com>
2022-10-21 11:31:55 -0400

    Poll and Vote request and repositories added

---
+++ API/src/main/java/com/bahadir/springreact/model/Vote.java
@@ -0,0 +1,41 @@
+package com.bahadir.springreact.model;
+
+import com.bahadir.springreact.model.audit.DateAudit;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(
+        name = "votes",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = {
+                                "poll_id",
+                                "user_id"
+                        }
+                )
+        }
+)
+public class Vote extends DateAudit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "poll_id", nullable = false)
+    private Poll poll;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "choice_id", nullable = false)
+    private Choice choice;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
---
+++ API/src/main/java/com/bahadir/springreact/payload/VoteRequest.java
@@ -0,0 +1,15 @@
+package com.bahadir.springreact.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class VoteRequest {
+
+    @NotNull
+    private Long choiceId;
+
+}
